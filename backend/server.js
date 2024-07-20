d23d3041a97ecc2de3abe9c5e4a3b9757568b8c6
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-20 19:55:44 +0530

    Add Get Users for Sidebar Endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -13,6 +16,7 @@ app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
