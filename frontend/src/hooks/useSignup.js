64936195feaf707ab74b5073730990282bc29622
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-23 21:06:47 +0530

    Add Protected Routes

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
   const signup = async ({
     fullName,
     username,
@@ -33,6 +35,13 @@ const useSignup = () => {
       });
 
       const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      //localstorage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      //context
+      setAuthUser(data);
       console.log(data);
     } catch (error) {
       toast.error(error.message);
