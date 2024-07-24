cd84821b575024515c1800a729925020729bfc7a
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-24 14:07:27 +0530

    Add Login Functionality

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
+import useLogin from "../../hooks/useLogin";
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const { loading, login } = useLogin()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(username, password);
+
+    }
     return (
         <div className="flex flex-col items-center justify-center mx-auto min-w-96 ">
             <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg">
@@ -11,13 +24,15 @@ function Login() {
 
                     </span>
                 </h1>
-                <form >
+                <form onSubmit={handleSubmit} >
                     <div>
                         <label className="p-2 label">
                             <span className="text-base label-text">Username</span>
 
                         </label>
-                        <input type="text" placeholder="Enter username" className="w-full h-10 input input-bordered" />
+                        <input type="text" placeholder="Enter username" className="w-full h-10 input input-bordered"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
 
                     </div>
                     <div>
@@ -25,13 +40,18 @@ function Login() {
                             <span className="text-base label-base">Password</span>
                         </label>
                         <input type="password" placeholder="Enter Password"
-                            className="w-full h-10 input input-border" />
+                            className="w-full h-10 input input-border"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <Link to='/signup' className="inline-block mt-2 text-sm hover:underline hover:text-blue-600">
                         {"Don't"} have an account ?
                     </Link>
                     <div>
-                        <button className="mt-2 btn btn-block btn-sm">Login</button>
+                        <button className="mt-2 btn btn-block btn-sm"
+                            disabled={loading}>
+                            {loading ? <span className="loading loading-spinner"></span> : "Login"}
+                        </button>
                     </div>
 
 
