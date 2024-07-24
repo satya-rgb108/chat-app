6af86fcd89180a3687141560d88781af4a721ce0
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-24 20:33:54 +0530

    Add Get Conversations Functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,16 +2,23 @@
 import Messages from "./Messages"
 import MessageInput from "./MessageInput"
 import { TiMessages } from "react-icons/ti"
+import useConversation from "../../zustand/useConversation"
+import { useEffect } from "react"
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    useEffect(() => {
+        //cleanup function (unmounts);
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
     return (
         <div className="md:min-w-[450px] flex flex-col">
-            {noChatSelected ? (<NoChatSelected />) : (
+            {!selectedConversation ? (<NoChatSelected />) : (
                 <>
                     {/*Header */}
                     <div className="px-4 py-2 mb-2 bg-slate-500">
                         <span className="label-text">To:</span>
-                        <span className="font-bold text-gray-900">John doe</span>
+                        <span className="font-bold text-gray-900">{selectedConversation.fullName}</span>
 
                     </div>
                     <Messages />
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,18 @@
-
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation"
 const Conversations = () => {
+    const { loading, conversations } = useGetConversations();
+
+
     return (
         <div className="flex flex-col py-2 overflow-auto">
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map((conversation, idx) => (<Conversation
+                key={conversation._id}
+                conversation={conversation}
+                emoji={getRandomEmoji()}
+                lastIdx={idx === conversations.length - 1} />))}
+            {loading ? <span className="mx-auto loading loading-spinner"></span> : null}
         </div>
     )
 }
