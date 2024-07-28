1cc4b3f702f0dfc84aa0369a036a9097de0a26ed
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-29 01:06:27 +0530

    fix minor bug

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads;
 app.use(cookieParser()); // to parse the incoming cookies from req.cookies
